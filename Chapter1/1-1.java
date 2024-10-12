c4b482c52cdc248427aa760efa4e9c0ce8616897
OliviaDev7 <2750905096@qq.com>
2024-10-12 18:27:54 +0800

    Update 1-1.java

--- Chapter1/1-1.java
+++ Chapter1/1-1.java
@@ -1,1 +1,7 @@
-
+public class Homework1_1 {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
