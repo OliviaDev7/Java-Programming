4e7fcbd22e912360274973f075bf08cce85f8416
OliviaDev7 <2750905096@qq.com>
2024-10-14 19:20:21 +0800

    Add files via upload

---
+++ Week6/Example/EP7Test.java
@@ -0,0 +1,23 @@
+public class EP7Test {
+    public static void main(String[] args){
+        int sum = 0;
+        int j = 0;
+        int i;
+        for (i = 0; i < 10; i++);
+        sum += i;
+
+        if(i < j)
+        System.out.println(i);
+                else
+                    System.out.println(j);
+
+                while (j < 10)
+        {
+            j++;
+        }
+
+        do{
+            j++;
+        }while(j < 10);
+    }
+}
